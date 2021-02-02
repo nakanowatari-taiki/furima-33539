1712afc398805d38c3330218a7bbddf64c2844c5
nakanowatari-taiki <starrito@icloud.com>
2021-02-02 15:57:09 +0900

    商品出品機能テストコード

---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    product_name          {"商品名"}
+    price                 {"1111"}
+    text                  {"hoge"}
+    user_id               {"hoge"}
+    state_id              {"2"}
+    categore_id           {"2"}
+    delivery_burden_id    {"2"}
+    delivery_area_id      {"2"}
+    delivery_days_id      {"2"}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image_png')
+  end
+end
+end
